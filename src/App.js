73c259ac7eeae3dc76bf73f7acf876017a172704
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2023-12-20 22:02:54 -0600

    added basic CSS properties

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Welcome to my Website</h1>
       </header>
       <article>
-        <p><a href="./about.js">About me</a></p>
-        <p><a href ="./experience">Experience</a></p>
+        <p><a href="./about.js">About me </a></p>
+        <p><a href ="./experience">Experience </a></p>
         <p><a href= "./projects">Project</a></p>
       </article>
     </div>
